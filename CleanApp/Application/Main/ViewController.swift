e9b65b882716c64a154db1a27ae63fc279fec715
Oscar De Moya <oscar.demoya@koombea.com>
2022-10-08 17:20:26 -0500

    Add code sample using Networking

--- CleanApp/Application/Main/ViewController.swift
+++ CleanApp/Application/Main/ViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import Networking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let api = TypicodeAPI()
+        let request = Request<[Post]>(path: "/posts")
+        let client = APIClient(api: api)
+        Task {
+            do {
+                let posts: [Post] = try await client.send(request: request)
+                print(posts)
+            } catch {
+                print(error)
+            }
+        }
     }
 
+}
+
+enum Environment {
+    case production
+}
+
+struct TypicodeAPI: API {
+    var environment: Environment
+    var baseURL: URL { "https://jsonplaceholder.typicode.com" }
+    
+    init(environment: Environment = .production) {
+        self.environment = environment
+    }
+}
+
+struct Post: Decodable {
+    var id: Int
+    var userId: Int
+    var title: String
+    var body: String
 }
